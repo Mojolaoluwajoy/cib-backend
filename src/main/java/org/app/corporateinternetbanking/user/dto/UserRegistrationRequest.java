35cb213b20b4e712f8b508cefe1f0796e62e28d7
Mojolaoluwajoy <adebolamojolaoluwajoy@gmail.com>
2026-05-23 01:10:47 +0100

    remove request for role,status and organization id

--- src/main/java/org/app/corporateinternetbanking/user/dto/UserRegistrationRequest.java
+++ src/main/java/org/app/corporateinternetbanking/user/dto/UserRegistrationRequest.java
@@ -2,21 +2,16 @@ package org.app.corporateinternetbanking.user.dto;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.app.corporateinternetbanking.user.enums.UserRole;
-import org.app.corporateinternetbanking.user.enums.UserStatus;
 
 @Setter
 @Getter
 public class UserRegistrationRequest {
     private String firstName;
     private String lastName;
-     private String email;
-      private String password;
-      private String nin;
-      private String adminKey;
-      private Long organizationId;
-   private UserRole role;
-   private UserStatus status;
-   private String token;
+    private String email;
+    private String password;
+    private String nin;
+    private String adminKey;
+    private String token;
 
 }
