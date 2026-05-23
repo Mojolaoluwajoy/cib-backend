d1fbfc27355fc88b5e9138fa79b9f8f6a0a08f02
Mojolaoluwajoy <adebolamojolaoluwajoy@gmail.com>
2026-05-23 01:11:08 +0100

    add role to request

--- src/main/java/org/app/corporateinternetbanking/user/dto/InvitationRequest.java
+++ src/main/java/org/app/corporateinternetbanking/user/dto/InvitationRequest.java
@@ -2,10 +2,12 @@ package org.app.corporateinternetbanking.user.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.app.corporateinternetbanking.user.enums.UserRole;
 
 @Setter
 @Getter
 public class InvitationRequest {
 
     private String userEmail;
+    private UserRole role;
 }
