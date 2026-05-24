a858546ff6cc3498574dcb688aa9e0d7d55e42df
Mojolaoluwajoy <adebolamojolaoluwajoy@gmail.com>
2026-05-24 18:42:19 +0100

    update FundingMap

--- src/main/java/org/app/corporateinternetbanking/transaction/utils/mapper/FundingMap.java
+++ src/main/java/org/app/corporateinternetbanking/transaction/utils/mapper/FundingMap.java
@@ -1,17 +1,23 @@
 package org.app.corporateinternetbanking.transaction.utils.mapper;
 
+import lombok.RequiredArgsConstructor;
 import org.app.corporateinternetbanking.transaction.dto.FundRequest;
 import org.app.corporateinternetbanking.transaction.dto.TransferRequest;
 import org.app.corporateinternetbanking.transaction.enums.TransactionType;
+import org.app.corporateinternetbanking.user.exceptions.UserNotFound;
+import org.app.corporateinternetbanking.user.service.UserServiceImpl;
+import org.springframework.stereotype.Component;
 
+@RequiredArgsConstructor
+@Component
 public class FundingMap {
+    private final UserServiceImpl userService;
 
-    public static TransferRequest mapToFundingRequest(FundRequest fundRequest) {
+    public TransferRequest mapToFundingRequest(FundRequest fundRequest) throws UserNotFound {
         TransferRequest transferRequest = new TransferRequest();
 
         transferRequest.setDestinationAccount(fundRequest.getAccountNumber());
         transferRequest.setAmount(fundRequest.getAmount());
-        transferRequest.setCreatorId(fundRequest.getCreatedBy());
         transferRequest.setType(TransactionType.EXTERNAL_FUNDING);
         transferRequest.setIdempotencyKey(fundRequest.getIdempotencyKey());
         return transferRequest;
