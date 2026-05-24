45c0e1b75ce5bd1568f90bcc9ad2eaf3b8d8a13e
Mojolaoluwajoy <adebolamojolaoluwajoy@gmail.com>
2026-05-24 18:44:51 +0100

    update PaymentServiceImpl

--- src/main/java/org/app/corporateinternetbanking/transaction/service/PaymentServiceImpl.java
+++ src/main/java/org/app/corporateinternetbanking/transaction/service/PaymentServiceImpl.java
@@ -1,16 +1,13 @@
 package org.app.corporateinternetbanking.transaction.service;
 
 import lombok.RequiredArgsConstructor;
-import org.app.corporateinternetbanking.account.domain.entity.Account;
 import org.app.corporateinternetbanking.account.exception.AccountDoesNotExist;
 import org.app.corporateinternetbanking.account.exception.InvalidAccount;
 import org.app.corporateinternetbanking.account.service.AccountService;
 import org.app.corporateinternetbanking.integration.paystack.PayStackClient;
-import org.app.corporateinternetbanking.transaction.domain.entity.Transaction;
 import org.app.corporateinternetbanking.transaction.dto.FundRequest;
 import org.app.corporateinternetbanking.transaction.dto.TransactionResponse;
 import org.app.corporateinternetbanking.transaction.dto.TransferRequest;
-import org.app.corporateinternetbanking.transaction.enums.TransactionType;
 import org.app.corporateinternetbanking.transaction.exceptions.DuplicateTransaction;
 import org.app.corporateinternetbanking.transaction.exceptions.InsufficientBalance;
 import org.app.corporateinternetbanking.transaction.exceptions.InvalidAmount;
@@ -23,8 +20,6 @@ import org.springframework.stereotype.Service;
 import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.UUID;
-import static org.app.corporateinternetbanking.transaction.utils.mapper.FundingMap.*;
 
 
 @Service
@@ -35,20 +30,21 @@ public class PaymentServiceImpl implements PaymentService {
 
     private final TransactionService transactionService;
     private final PayStackClient payStackClient;
+    private final FundingMap fundingMap;
 
     @Override
     public Map<String, Object> initializeFunding(FundRequest fundRequest) throws IsNull, AccountDoesNotExist, UserNotFound, InvalidAccount, InvalidAmount, InsufficientBalance, UnauthorizedAccess, DuplicateTransaction {
 
-        TransferRequest  transferRequest= mapToFundingRequest(fundRequest);
+        TransferRequest transferRequest = fundingMap.mapToFundingRequest(fundRequest);
 
-        TransactionResponse response=transactionService.initiateTransaction(transferRequest);
+        TransactionResponse response = transactionService.initiateTransaction(transferRequest);
 
-        Map<String,Object> body=new HashMap<>();
+        Map<String, Object> body = new HashMap<>();
 
-        body.put("email",fundRequest.getEmail());
-        body.put("amount",fundRequest.getAmount().multiply(BigDecimal.valueOf(100)));
+        body.put("email", fundRequest.getEmail());
+        body.put("amount", fundRequest.getAmount().multiply(BigDecimal.valueOf(100)));
 
-        body.put("reference",response.getTransactionReference());
+        body.put("reference", response.getTransactionReference());
         return payStackClient.initializeTransaction(body);
 
 
