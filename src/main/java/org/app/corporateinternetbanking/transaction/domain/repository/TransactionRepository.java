1cbf01c0d98a25c7f09e3d4ebf0459fec5b90feb
Mojolaoluwajoy <adebolamojolaoluwajoy@gmail.com>
2026-05-25 00:42:45 +0100

    add find all by status.

--- src/main/java/org/app/corporateinternetbanking/transaction/domain/repository/TransactionRepository.java
+++ src/main/java/org/app/corporateinternetbanking/transaction/domain/repository/TransactionRepository.java
@@ -1,7 +1,7 @@
 package org.app.corporateinternetbanking.transaction.domain.repository;
 
-import org.app.corporateinternetbanking.transaction.enums.TransactionStatus;
 import org.app.corporateinternetbanking.transaction.domain.entity.Transaction;
+import org.app.corporateinternetbanking.transaction.enums.TransactionStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,7 +10,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
-public interface TransactionRepository extends JpaRepository<Transaction,Long> {
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 
 
     List<Transaction> findByStatus(TransactionStatus status);
@@ -27,5 +27,5 @@ public interface TransactionRepository extends JpaRepository<Transaction,Long> {
     List<Transaction> findByCreatedAtBetweenAndStatus(LocalDateTime start, LocalDateTime end, TransactionStatus status);
 
 
-
+    List<Transaction> findAllByStatus(TransactionStatus transactionStatus);
 }
