53be98b5631388244a9301e58d24c5f0867609b9
Mojolaoluwajoy <adebolamojolaoluwajoy@gmail.com>
2026-05-24 18:47:10 +0100

    update TransactionApprovalRequest

--- src/main/java/org/app/corporateinternetbanking/transaction/dto/TransactionApprovalRequest.java
+++ src/main/java/org/app/corporateinternetbanking/transaction/dto/TransactionApprovalRequest.java
@@ -3,10 +3,11 @@ package org.app.corporateinternetbanking.transaction.dto;
 import lombok.Getter;
 import lombok.Setter;
 import org.app.corporateinternetbanking.transaction.enums.TransactionStatus;
+
 @Setter
 @Getter
 public class TransactionApprovalRequest {
     private long transactionId;
     private TransactionStatus status;
-    private Long approverId;
+
 }
