5c4a88a400a92afc1806c582fe9cb227d7283111
Mojolaoluwajoy <adebolamojolaoluwajoy@gmail.com>
2026-05-25 00:42:56 +0100

    add find by status.

--- src/main/java/org/app/corporateinternetbanking/account/domain/repository/AccountRepository.java
+++ src/main/java/org/app/corporateinternetbanking/account/domain/repository/AccountRepository.java
@@ -1,8 +1,10 @@
 package org.app.corporateinternetbanking.account.domain.repository;
 
 import org.app.corporateinternetbanking.account.domain.entity.Account;
+import org.app.corporateinternetbanking.organization.domain.entity.Organization;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
@@ -10,4 +12,5 @@ public interface AccountRepository extends JpaRepository<Account, Long> {
 
     Optional<Account> findByAccountNumber(String accountNumber);
 
+    List<Account> findAllByOrganization(Organization org);
 }
