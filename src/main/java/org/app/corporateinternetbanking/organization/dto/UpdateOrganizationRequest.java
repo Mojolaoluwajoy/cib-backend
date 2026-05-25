23306ab9850826812284ef85385505fd31de0067
Mojolaoluwajoy <adebolamojolaoluwajoy@gmail.com>
2026-05-25 21:59:21 +0100

    create and populate update organization request class.

---
+++ src/main/java/org/app/corporateinternetbanking/organization/dto/UpdateOrganizationRequest.java
@@ -0,0 +1,15 @@
+package org.app.corporateinternetbanking.organization.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.app.corporateinternetbanking.organization.enums.OrganizationStatus;
+
+@Setter
+@Getter
+public class UpdateOrganizationRequest {
+    private String name;
+    private String organizationEmail;
+    private String registrationNumber;
+    private OrganizationStatus organizationStatus;
+    
+}
