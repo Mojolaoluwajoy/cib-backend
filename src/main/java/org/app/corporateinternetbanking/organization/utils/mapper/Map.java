95a7e3001d5cc2fa3ef31dfc1b161da59ba0e2ba
Mojolaoluwajoy <adebolamojolaoluwajoy@gmail.com>
2026-05-26 23:14:28 +0100

    new update.

--- src/main/java/org/app/corporateinternetbanking/organization/utils/mapper/Map.java
+++ src/main/java/org/app/corporateinternetbanking/organization/utils/mapper/Map.java
@@ -1,22 +1,28 @@
 package org.app.corporateinternetbanking.organization.utils.mapper;
 
+import lombok.RequiredArgsConstructor;
 import org.app.corporateinternetbanking.organization.domain.entity.Organization;
 import org.app.corporateinternetbanking.organization.dto.OrganizationId;
 import org.app.corporateinternetbanking.organization.dto.OrganizationOnlyResponse;
 import org.app.corporateinternetbanking.organization.dto.OrganizationRegistrationResponse;
 import org.app.corporateinternetbanking.organization.dto.OrganizationRequest;
 import org.app.corporateinternetbanking.user.domain.entity.User;
+import org.app.corporateinternetbanking.user.domain.repository.UserRepository;
+import org.app.corporateinternetbanking.user.enums.UserRole;
 
 import java.util.UUID;
 
+@RequiredArgsConstructor
 public class Map {
+    private final UserRepository userRepository;
+
     public static Organization mapRequest(OrganizationRequest request) {
         Organization organization = new Organization();
 
         organization.setName(request.getName());
         organization.setRegistrationNumber(request.getRegistrationNumber());
         organization.setOrganizationEmail(request.getOrganizationEmail());
-
+        organization.setPhoneNumber(request.getPhoneNumber());
         return organization;
     }
 
@@ -49,12 +55,16 @@ public class Map {
         user.setEmail(registrationRequest.getEmail());
         user.setNin(registrationRequest.getNin());
         user.setPassword(registrationRequest.getPassword());
+
         return user;
     }
 
-    public static OrganizationOnlyResponse mapResponse(Organization organization) {
+    public OrganizationOnlyResponse mapResponse(Organization organization) {
         OrganizationOnlyResponse response = new OrganizationOnlyResponse();
-
+        User admin = userRepository
+                .findByOrganizationAndRole(organization, UserRole.ADMIN)
+                .orElse(null);
+        response.setAdminId(admin != null ? admin.getId() : null);
         response.setId(organization.getId());
         response.setName(organization.getName());
         response.setRegistrationNumber(organization.getRegistrationNumber());
