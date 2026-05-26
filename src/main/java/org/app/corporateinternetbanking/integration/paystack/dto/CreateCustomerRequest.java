8e04eefa17b03be2f67cb32a56f12197eebcfe48
Mojolaoluwajoy <adebolamojolaoluwajoy@gmail.com>
2026-05-26 21:49:19 +0100

    create and populatr create customer request dto.

---
+++ src/main/java/org/app/corporateinternetbanking/integration/paystack/dto/CreateCustomerRequest.java
@@ -0,0 +1,21 @@
+package org.app.corporateinternetbanking.integration.paystack.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+public class CreateCustomerRequest {
+    private String email;
+
+    @JsonProperty("first_name")
+    private String firstName;
+
+    @JsonProperty("last_name")
+    private String lastName;
+
+    //  private String phone;
+}
