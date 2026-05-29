6849b08ad01ceb505b859ce7ff9fd8eff8fab82b
Mojolaoluwajoy <adebolamojolaoluwajoy@gmail.com>
2026-05-29 19:31:56 +0100

    add createAccountForOrg method.

--- src/main/java/org/app/corporateinternetbanking/account/service/AccountService.java
+++ src/main/java/org/app/corporateinternetbanking/account/service/AccountService.java
@@ -4,12 +4,12 @@ import org.app.corporateinternetbanking.account.domain.entity.Account;
 import org.app.corporateinternetbanking.account.dto.AccountRequest;
 import org.app.corporateinternetbanking.account.dto.AccountResponse;
 import org.app.corporateinternetbanking.account.exception.AccountDoesNotExist;
-import org.app.corporateinternetbanking.transaction.exceptions.InsufficientBalance;
-import org.app.corporateinternetbanking.transaction.exceptions.IsNull;
-import org.app.corporateinternetbanking.user.exceptions.UserNotFound;
 import org.app.corporateinternetbanking.currency.exceptions.CurrencyNotActive;
 import org.app.corporateinternetbanking.currency.exceptions.CurrencyNotFound;
 import org.app.corporateinternetbanking.organization.exceptions.OrganizationDoesNotExist;
+import org.app.corporateinternetbanking.transaction.exceptions.InsufficientBalance;
+import org.app.corporateinternetbanking.transaction.exceptions.IsNull;
+import org.app.corporateinternetbanking.user.exceptions.UserNotFound;
 
 import java.math.BigDecimal;
 import java.util.List;
@@ -17,13 +17,17 @@ import java.util.List;
 public interface AccountService {
     AccountResponse createAccount(AccountRequest request) throws OrganizationDoesNotExist, UserNotFound, CurrencyNotFound, CurrencyNotActive;
 
-   List <AccountResponse> viewAll();
+    List<AccountResponse> viewAll();
 
-   AccountResponse findById(Long id)throws AccountDoesNotExist;
+    AccountResponse findById(Long id) throws AccountDoesNotExist;
 
 
-   void credit(Long accountId, BigDecimal amount) throws AccountDoesNotExist;
-   void debit(Long accountId, BigDecimal amount) throws AccountDoesNotExist, InsufficientBalance;
+    void credit(Long accountId, BigDecimal amount) throws AccountDoesNotExist;
 
-Account getValidAccount(String accountNumber) throws AccountDoesNotExist, IsNull;
+    void debit(Long accountId, BigDecimal amount) throws AccountDoesNotExist, InsufficientBalance;
+
+    Account getValidAccount(String accountNumber) throws AccountDoesNotExist, IsNull;
+
+    AccountResponse createAccountForOrg(Long orgId, AccountRequest request)
+            throws OrganizationDoesNotExist, UserNotFound, CurrencyNotFound, CurrencyNotActive;
 }
