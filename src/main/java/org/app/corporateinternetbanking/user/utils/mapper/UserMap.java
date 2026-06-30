c462d868df7222b68ea0e81e882e4d74f544fe30
Mojolaoluwajoy <adebolamojolaoluwajoy@gmail.com>
2026-06-30 23:32:22 +0100

    remove unused methods

--- src/main/java/org/app/corporateinternetbanking/user/utils/mapper/UserMap.java
+++ src/main/java/org/app/corporateinternetbanking/user/utils/mapper/UserMap.java
@@ -2,26 +2,10 @@ package org.app.corporateinternetbanking.user.utils.mapper;
 
 import org.app.corporateinternetbanking.organization.dto.OrganizationId;
 import org.app.corporateinternetbanking.user.domain.entity.User;
-import org.app.corporateinternetbanking.user.dto.UserRegistrationRequest;
 import org.app.corporateinternetbanking.user.dto.UserResponse;
-import org.app.corporateinternetbanking.user.enums.UserStatus;
-
-import java.util.UUID;
 
 public class UserMap {
 
-
-    public static User userMapRequest(UserRegistrationRequest registrationRequest) {
-        User user = new User();
-        user.setStatus(UserStatus.INACTIVE);
-        user.setUserId(UUID.randomUUID().toString());
-        user.setFirstName(registrationRequest.getFirstName());
-        user.setLastName(registrationRequest.getLastName());
-        user.setEmail(registrationRequest.getEmail());
-        user.setNin(registrationRequest.getNin());
-        user.setNin(registrationRequest.getNin());
-        return user;
-    }
 
     public static UserResponse mapResponse(User user) {
         UserResponse response = new UserResponse();
